fd26d4e2f3c94a1344b1d826e2be2a630f8a16f0
seramat <exodus.bo@gmail.com>
2018-03-10 14:27:47 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@ var __apiKeys = [
 	'm780103789-d887d129dc783174efaf4e7b', //ssrto
 	'm780103788-2d612c9d4931117cc796e4fb', //ssvpn
 	'm780103790-3160ac586f416b3926ada1cb', //menghan
-//	'm778126978-24e6f1ce275c2518bad1e925', //Update B
-//	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
-//	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
+	'm780103801-a03e134723c21af762cf1a0a', //Ecnotus
+	'm780103799-0d2adca9f93f75a134d5e410', //AlphaRack
+	'm780103797-4edd7a8c729ef28b175dd7a2', //Info
 //	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
 //	'm776858321-486827042ed7a7f7b8952a05', //Auth B
 //	'm778734196-a51947f061c5db10cee048f7', //Auth C
