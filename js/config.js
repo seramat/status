ed06b3c0a617f3c6b090ddb4a1ed2653c9e7df9f
seramat <exodus.bo@gmail.com>
2018-03-10 14:21:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,14 +4,14 @@ var __apiKeys = [
 	'm780103789-d887d129dc783174efaf4e7b', //ssrto
 	'm780103788-2d612c9d4931117cc796e4fb', //ssvpn
 	'm780103790-3160ac586f416b3926ada1cb', //menghan
-	'm778126978-24e6f1ce275c2518bad1e925', //Update B
-	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
-	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
-	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
-	'm776858321-486827042ed7a7f7b8952a05', //Auth B
-	'm778734196-a51947f061c5db10cee048f7', //Auth C
-	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
-	'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
+//	'm778126978-24e6f1ce275c2518bad1e925', //Update B
+//	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
+//	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
+//	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
+//	'm776858321-486827042ed7a7f7b8952a05', //Auth B
+//	'm778734196-a51947f061c5db10cee048f7', //Auth C
+//	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
+//	'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
 ];
 
 // refresh interval (in seconds)
