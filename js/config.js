b2f65b6a84c8eef20e7cb1ca0d7bbabebcb5fd17
seramat <exodus.bo@gmail.com>
2020-03-31 14:28:15 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,17 +1,20 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm780103787-ec2d2ec9eaffd3f86d9294d2', //Site
-	'm780103789-d887d129dc783174efaf4e7b', //ssrto
-	'm780103788-2d612c9d4931117cc796e4fb', //ssvpn
-	'm780103790-3160ac586f416b3926ada1cb', //menghan
-	'm780103801-a03e134723c21af762cf1a0a', //Ecnotus
-	'm780103799-0d2adca9f93f75a134d5e410', //AlphaRack
-	'm780103797-4edd7a8c729ef28b175dd7a2', //Info
-//	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
-//	'm776858321-486827042ed7a7f7b8952a05', //Auth B
-//	'm778734196-a51947f061c5db10cee048f7', //Auth C
-//	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
-//	'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
+                'm780103787-ec2d2ec9eaffd3f86d9294d2', //GX_Win
+                'm780103788-2d612c9d4931117cc796e4fb', //ssvpn
+                'm784501408-49f6a948c7ce1c44f87e0707', //vmw_SSR
+                'm780532023-c87c8e56027eb723b0cd3500', //tieren_V2Ray
+                'm780230079-1f73aa62413d64f5c0e34046', //tieren_SSR
+                'm784501424-59b3d6cf4a87339f3baba2ba', //so_V2Ray
+                'm784501369-016517d0de2e85adad3fa923', //pac_V2Ray
+                'm784501383-97d6296257f17573ace8671f', //pac2_V2Ray
+                'm780103790-3160ac586f416b3926ada1cb', //menghan_SSR
+                'm784501391-a160bebf57b8e7950e56a345', //menghan_V2Ray
+                'm784501351-bc7de27ef1ba487924f9b70f', //fly_V2Ray
+                'm784501386-44270e839aebe3f0e212f1de', //eon_V2Ray
+                'm780103789-d887d129dc783174efaf4e7b', //az_V2Ray
+                'm780230122-3eb08cb9006e49353076a51c', //PandaImage
+
 ];
 
 // refresh interval (in seconds)
